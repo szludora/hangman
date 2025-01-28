00943b4c6c94da4a9431799dd80cf6cd58c002d7
szludora <szlucska.dora@gmail.com>
2025-01-28 13:51:04 +0100

    template

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
-  )
+  return <>
+  </>;
 }
 
-export default App
+export default App;
